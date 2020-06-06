113448fdedf1ced29c7e1856fe1224e0db389eea
Jumpei Ogawa <phanective@gmail.com>
2020-06-06 20:55:40 +0900

    chore: set missing tsconfig.json for test dir

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const { join } = require("path");
+
 module.exports = {
   extends: "plugin:@phanect/jest",
+  parserOptions: {
+    project: join(__dirname, "./tsconfig.json"),
+  },
 };
